ea625d6fd168da5d88ad86751067a0e5f305132d
Ghulam Tahir <gtdev6@gmail.com>
2024-07-17 18:39:45 +0500

    updated files

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,20 +1,8 @@
 import { defineConfig } from "vite";
-import envPlugin from "@vitejs/plugin-env";
+// import envPlugin from "@vitejs/plugin-env";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-        plugins: [
-                react(),
-                envPlugin({
-                        // Loads environment variables based on NODE_ENV
-                        // For development: loads from .env
-                        // For production: loads from .env.production
-                        dotenv:
-                                ".env" +
-                                (process.env.NODE_ENV === "production"
-                                        ? ".production"
-                                        : ""),
-                }),
-        ],
+        plugins: [react()],
 });
